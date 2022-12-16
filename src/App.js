e2c9419f2310e9f7442c8ca20114e35871130dc3
spthedevloper1012 <MsOffice10122002@gmail.com>
2022-12-16 10:44:30 +0530

    Smit Patel

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './cmp/Navbar';
+import Header from './cmp/Header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/cmp/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+        <div className='main-info'>
+            <h1>
+                web development and digital marketing
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
